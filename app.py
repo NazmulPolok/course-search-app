f7470269ee3341450a999fd2a7e305b79a2a4a7f
Nazmul.Polok <nazmulpolok80@gmail.com>
2026-09-24 01:57:14 +0600

    Refactor app.py for improved functionality and clarity

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ if df is not None:
                                     """
 
                                     response = client.models.generate_content(
-                                        model='gemini-2.5-flash',
+                                        model='gemini-1.5-flash',
                                         contents=prompt,
                                     )
                                     st.info(response.text)
